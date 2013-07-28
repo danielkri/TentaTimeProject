d76c510a7e30ac686750087c309099549ec6d576
Joseph Hejderup <j.i.hejderup@student.tudelft.nl>
2013-07-28 18:07:08 +0200

    Using requests instead of urllib as it has improved speed and readability

--- examdate_restapi_python/app/parser.py
+++ examdate_restapi_python/app/parser.py
@@ -2,21 +2,22 @@
 # A: jhejdeurp
 # TentaTimeProject
 from lxml import html
-import urllib
+import requests
 
 
 #This URL retrieves all course codes that contains a "D" and the list is sorted on Course codes
 test_url = "https://www.student.chalmers.se/sp/examdates_list?flag=1;sortorder=C_CODE,CM_CODE,EX_DATE;search_course_code=dat050"
 
-#Get the content
-content = urllib.urlopen(test_url).read()
-doc = html.fromstring(content)
-doc.make_links_absolute(test_url)
+#Get the webpage
+page = requests.get(test_url)
+#Create a structured document
+doc = html.fromstring(page.text)
 
-content = doc.xpath('.//*[@id="contentpage"]//table[3]/tr[not(@class="tableHeader") and position()>1]')
+#Get a list <td> elements for each exam
+#The first row is removed as well as the tableheader
+examdates_list = doc.xpath('.//*[@id="contentpage"]//table[3]/tr[not(@class="tableHeader") and position()>1]')
 
 
-
-print html.tostring(content[0])
+print html.tostring(examdates_list[0])
 
 

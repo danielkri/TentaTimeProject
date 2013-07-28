99ef0004c16b2b20f1591980709d21b7e360a3b3
Joseph Hejderup <j.i.hejderup@student.tudelft.nl>
2013-07-28 18:02:11 +0200

    Using XPath to retrive the exam information, we get all table elements

--- examdate_restapi_python/app/parser.py
+++ examdate_restapi_python/app/parser.py
@@ -1,19 +1,22 @@
 # Proof of Concept - Parse a Studentportal HTML file
 # A: jhejdeurp
 # TentaTimeProject
-import lxml.html
-from lxml.html import fromstring
-from lxml import etree
+from lxml import html
 import urllib
+
+
 #This URL retrieves all course codes that contains a "D" and the list is sorted on Course codes
-test_url = "https://www.student.chalmers.se/sp/examdates_list?flag=1;sortorder=C_CODE,CM_CODE,EX_DATE;search_course_code=d"
+test_url = "https://www.student.chalmers.se/sp/examdates_list?flag=1;sortorder=C_CODE,CM_CODE,EX_DATE;search_course_code=dat050"
+
 #Get the content
 content = urllib.urlopen(test_url).read()
-#Reconstruct the content to a HTML doc
-doc = fromstring(content)
+doc = html.fromstring(content)
 doc.make_links_absolute(test_url)
 
-#Retrieve the element with contentpage
-el = doc.get_element_by_id('contentpage')
+content = doc.xpath('.//*[@id="contentpage"]//table[3]/tr[not(@class="tableHeader") and position()>1]')
 
-print el.text_content()
+
+
+print html.tostring(content[0])
+
+

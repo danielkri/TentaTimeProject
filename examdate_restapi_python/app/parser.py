1b51f37aa79f4a8aee16d2238edf31b61346db5e
Joseph Hejderup <j.i.hejderup@student.tudelft.nl>
2013-07-28 19:00:19 +0200

    extractes all values from an exam

--- examdate_restapi_python/app/parser.py
+++ examdate_restapi_python/app/parser.py
@@ -5,8 +5,10 @@ from lxml import html
 import requests
 
 
+# TODO: fixa fallet med DAT042 som har ett varningmarke pa den sista <td>
+
 #This URL retrieves all course codes that contains a "D" and the list is sorted on Course codes
-test_url = "https://www.student.chalmers.se/sp/examdates_list?flag=1;sortorder=C_CODE,CM_CODE,EX_DATE;search_course_code=dat050"
+test_url = "https://www.student.chalmers.se/sp/examdates_list?flag=1;sortorder=C_CODE,CM_CODE,EX_DATE;search_course_code=dat042"
 
 #Get the webpage
 page = requests.get(test_url)
@@ -17,7 +19,19 @@ doc = html.fromstring(page.text)
 #The first row is removed as well as the tableheader
 examdates_list = doc.xpath('.//*[@id="contentpage"]//table[3]/tr[not(@class="tableHeader") and position()>1]')
 
+for exam_el in examdates_list:
+	print exam_el.xpath('.//td[1]/a/text()')[0]
+	print exam_el.xpath('.//td[1]/a/@href')[0]	
+	print exam_el.xpath('.//td[2]/text()')[0]
+	print exam_el.xpath('.//td[2]/small/text()')[0]
+	print exam_el.xpath('.//td[3]/b/nobr/text()')[0]
+	print exam_el.xpath('.//td[4]/center/text()')[0]
+	print exam_el.xpath('.//td[5]/text()')[0]
+	print exam_el.xpath('.//td[6]/text()')[0]
+	print exam_el.xpath('.//td[7]/text()')[0]
+	print exam_el.xpath('.//td[7]/a/@href')
 
-print html.tostring(examdates_list[0])
+#print examdates_list
+#print html.tostring(examdates_list[0])
 
 

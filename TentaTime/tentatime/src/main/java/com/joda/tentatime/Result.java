1af7c16c14d7463ba8a0809299012cd13fd5aa32
Daniel Kristoffersson <dlkn87@gmail.com>
2013-12-28 11:42:58 +0100

    ListView working correct, display of exam now also working correct.

--- TentaTime/tentatime/src/main/java/com/joda/tentatime/Result.java
+++ TentaTime/tentatime/src/main/java/com/joda/tentatime/Result.java
@@ -1,10 +1,13 @@
 package com.joda.tentatime;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -16,9 +19,32 @@ import java.util.List;
  * Copyright (c) Joseph Hejderup & Daniel Kristoffersson, All rights reserved.
  * See License.txt in the project root for license information.
  */
-public class Result extends ListActivity {
+public class Result extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
 
-    public void onCreate(Bundle savedInstanceState) {
+        Bundle b = getIntent().getExtras();
+        ExamResult mExam = (ExamResult) b.getSerializable("key");
+
+        setupWidgets(mExam);
+    }
+
+    private void setupWidgets(ExamResult exam){
+        TextView name = (TextView) findViewById(R.id.examName); if (name == null) { Log.w("", "TextView is null"); }
+        name.append(exam.getName().trim());
+        TextView code = (TextView) findViewById(R.id.examCode); if (code == null) { Log.w("", "TextView is null"); }
+        code.append(exam.getCode());
+        TextView date = (TextView) findViewById(R.id.examDate); if (date == null) { Log.w("", "TextView is null"); }
+        date.append(exam.getExamDate());
+        TextView time = (TextView) findViewById(R.id.examTime); if (time == null) { Log.w("", "TextView is null"); }
+        time.append(exam.getBeginsAt());
+        TextView description = (TextView) findViewById(R.id.examDescription); if (description == null) { Log.w("", "TextView is null"); }
+        description.append(exam.getExamDate());
+    }
+
+    /*public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         Bundle b = getIntent().getExtras();
@@ -33,5 +59,5 @@ public class Result extends ListActivity {
         }
 
         setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, examNames));
-    }
+    }*/
 }
